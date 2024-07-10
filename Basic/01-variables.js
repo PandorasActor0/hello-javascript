cfd878ff1e0032f9257c09c910240a12a4e02f38
PandorasActor0 <nelsona2001@hotmail.com>
2024-07-10 14:47:53 -0500

    Se muestra como crear variables

--- Basic/01-variables.js
+++ Basic/01-variables.js
@@ -1,7 +1,24 @@
 //var
+var helloWorld = "hola, JavaScript!"
+console.log(helloWorld)
 
-var helloworld 
+helloWorld = "Hola de nuevo, JavaScript"
+console.log(helloWorld)
+
 
 //let
 
+let helloWorld2 = "hola, JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo, JavaScript 2"
+console.log(helloWorld2)
+
 //const
+
+const helloWorld3 = "Hola, JavaScript 3!"
+console.log(helloWorld3)
+
+//  Error
+//  helloWorld3 = "Hola de nuevo, JavaScrpt 3 !"
+//  console.log(helloWorld3)
